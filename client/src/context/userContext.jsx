13ad755e1685522d7a4d7bdf4365e8c2b3f81143
Sudhardeena <sudharsanam16499@gmail.com>
2024-09-08 12:52:38 +0530

    frontend updated context

--- client/src/context/userContext.jsx
+++ client/src/context/userContext.jsx
@@ -3,7 +3,8 @@ import {  createContext, useState } from "react";
 export  const UserContext = createContext()
 
 const UserContextProvider = (props) =>{
-    const user = "user"
+    const user = null
+    
     return (
         <UserContext.Provider value={user}>
             {props.children}
