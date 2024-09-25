0c17420c7dc15623c43c4dbb6c476cc29a09b269
Sudhardeena <sudharsanam16499@gmail.com>
2024-09-25 13:24:01 +0530

    added * to origin at end

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename); // get the name of the directory
 const app = express()
 
 app.use(cors({
-  origin: "https://blogger-app-five.vercel.app/", // Allow your frontend domain
+  origin: "https://blogger-app-five.vercel.app/*", // Allow your frontend domain
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Include headers you expect
 }));
