7d37eafbf892556eb7d738ff45f125287d0bd14e
Sudhardeena <sudharsanam16499@gmail.com>
2024-09-28 21:32:48 +0530

    testing moment timezone

--- client/src/components/BlogItem/BlogItem.jsx
+++ client/src/components/BlogItem/BlogItem.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './BlogItem.css'
 import { Link } from 'react-router-dom'
-import moment from 'moment'
-// import moment from 'moment-timezone';
+// import moment from 'moment'
+import moment from 'moment-timezone';
 
 import { UserContext } from '../../context/userContext'
 import { useContext } from 'react'
@@ -14,7 +14,7 @@ const BlogItem = (props) => {
     const {blogDate,desc,blogId,blogImg,title,username} = blogDetails
     // console.log(blogImg)
     const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    // const blogDateLocal = moment.utc(blogDate).tz(userTimezone).fromNow();
+    const blogDateLocal = moment.utc(blogDate).tz(userTimezone).fromNow();
     // console.log(userTimezone,blogDate,blogDateLocal) 
 
   return (
@@ -25,7 +25,7 @@ const BlogItem = (props) => {
         </div>
         <div className='blog-info-container'>
             <h1 className='blog-title'>{title}</h1>
-            <p className='username-and-date'><span className='blog-user-name'>{username}</span>    {moment(blogDate).fromNow()}</p>
+            <p className='username-and-date'><span className='blog-user-name'>{username}</span>    {blogDateLocal}</p>
             <p className='bolg-desc'>{desc.slice(0,100)}....</p>
         </div>
     </li>
