2fa0c789839032aa5e4d829f0c7d6331201b64f4
Sudhardeena <106978348+Sudhardeena@users.noreply.github.com>
2024-12-18 11:38:40 +0530

    Update Home.jsx

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import BlogList from '../../components/BlogList/BlogList';
 import {BsSearch} from 'react-icons/bs'
