afc9f684783be7f4030c8d329b661f2204074014
Sudhardeena <sudharsanam16499@gmail.com>
2024-12-22 03:57:52 +0530

    added docker and compose.yaml for both frontend and backend

--- client/src/context/userContext.jsx
+++ client/src/context/userContext.jsx
@@ -1,18 +1,18 @@
-import {  createContext, useState } from "react";
+import { createContext, useState } from "react";
 
-export  const UserContext = createContext()
+export const UserContext = createContext();
 
-const UserContextProvider = (props) =>{
-    const [user,setUser] = useState(null) 
-    const backendUrl = 'https://blogger-app-backend-haxv.onrender.com'
-    // const backendUrl = 'http://localhost:8000'
-    // console.log(user)
+const UserContextProvider = (props) => {
+  const [user, setUser] = useState(null);
+  const backendUrl = "https://blogger-app-backend-haxv.onrender.com";
+  //   const backendUrl = "http://localhost:8000";
+  // console.log(user)
 
-    return (
-        <UserContext.Provider value={{user,setUser,backendUrl}}>
-            {props.children}
-        </UserContext.Provider>
-    )
-}
+  return (
+    <UserContext.Provider value={{ user, setUser, backendUrl }}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
 
-export default UserContextProvider
+export default UserContextProvider;
